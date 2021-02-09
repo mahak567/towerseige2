9891f25dcdb0db548a5d2f8da84f748580c00e17
mahak567 <74048800+mahak567@users.noreply.github.com>
2021-02-09 19:52:11 +0530

    Add files via upload

---
+++ js/Block.js
@@ -0,0 +1,38 @@
+class Block{
+    constructor(x, y, width, height) {
+        var options = {
+           
+            restitution :0.4,
+            friction :0.0,
+            //isStatic:true
+           
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.Visiblity =205;
+        this.image=loadImage("tin transparent.png");
+        World.add(world, this.body);
+      }
+      display(){
+        console.log(this.body.speed);
+        if(this.body.speed <5){
+        var angle = this.body.angle;
+        var pos= this.body.position;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image,0,0,this.width, this.height);
+        pop();
+      } else{
+        World.remove(world, this.body);
+        push();
+        this.Visiblity = this.Visiblity - 5;
+        tint(255,this.Visiblity);
+        image(this.image, this.body.position.x, this.body.position.y, this.width, this.height);
+        pop();
+      }
+     
+    }
+}
